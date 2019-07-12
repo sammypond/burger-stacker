827403de28c7169c990498fa9132c1bba68791dc
Samuel Pond Marritz <kim@Samuels-MacBook-Air.local>
2019-07-12 09:02:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: [
+        {name: 'Kaiser Bun', color: 'saddlebrown'},
+        {name: 'Sesame Bun', color: 'sandybrown'},
+        {name: 'Gluten Free Bun', color: 'peru'},
+        {name: 'Lettuce Wrap', color: 'olivedrab'},
+        {name: 'Beef Patty', color: '#3F250B'},
+        {name: 'Soy Patty', color: '#3F250B'},
+        {name: 'Black Bean Patty', color: '#3F250B'},
+        {name: 'Chicken Patty', color: 'burlywood'},
+        {name: 'Lettuce', color: 'lawngreen'},
+        {name: 'Tomato', color: 'tomato'},
+        {name: 'Bacon', color: 'maroon'},
+        {name: 'Onion', color: 'lightyellow'}
+      ],
+      stack: []
+    }
+    this.addIngredient = this.addIngredient.bind(this); 
+  }
+
+  addIngredient (e) {
+    e.preventDefault();
+    let stack = Array.from(this.state.stack);
+    stack.push(this.state.ingredients);
+    this.setState({
+      stack
+    })
+  }
+
+  render() {
+  // let stack = Array.from(this.state.stack);
+
+    return (
+      <>
+      <div>
+      <container id="menu">
+      <input type="text"></input>
+      <button onClick={this.addIngredient}>Add</button>
+      </container>
+      </div>
+      </>
+    )
 }
+}
+
 
 export default App;
